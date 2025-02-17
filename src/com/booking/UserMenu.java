8253b932a507d38fd04a9a0788dae0877f2d1f71
user <user@DESKTOP-S6UUK9H>
2025-02-17 16:31:10 +0900

    기본세팅

---
+++ src/com/booking/UserMenu.java
@@ -0,0 +1,13 @@
+package com.booking;
+
+public class UserMenu { // 유저 메뉴 카테고리
+
+	static int answer;
+	
+	public void menu() {
+		
+		
+	}
+
+
+}
---
+++ src/com/booking/member/Admin.java
@@ -0,0 +1,18 @@
+package com.booking.member;
+
+public class Admin {
+	
+	String ID;
+	String passwd;
+	String email;
+	String name;
+	
+	public Admin(String iD, String passwd, String email, String name) {
+		super();
+		ID = iD;
+		this.passwd = passwd;
+		this.email = email;
+		this.name = name;
+	}
+
+}
